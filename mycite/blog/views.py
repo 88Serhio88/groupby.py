53d5875597fa283c81a580b8a8db12b95d1090b5
Sergey Zhilenkov <mr.darkusha8@gmail.com>
2023-10-05 22:54:11 +0300

    add send mail

--- mycite/blog/views.py
+++ mycite/blog/views.py
@@ -2,29 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage,\
                                             PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
-
-class PostListView(ListView):
-    """
-    Альтернативное представления списка постов
-    и использованием классов
-    """
-
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+from .forms import EmailPostForm
 
 def post_list(request):
     post_list = Post.published.all()
@@ -44,3 +24,57 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+class PostListView(ListView):
+    """
+    Альтернативное представления списка постов
+    и использованием классов
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id = post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        #Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read" \
+                    f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                    f"{cd['name']}\ 's comments: {cd['comments']}"
+            send_mail(subject, message, 'mr.darkusha8@gmail.com',
+                    [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                   'form': form,
+                                                   'sent': sent}) 
+
+
+
+
+
