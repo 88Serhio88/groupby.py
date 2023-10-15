75949c613b24aa4e117594523b5e42527dadd6ab
Sergey Zhilenkov <mr.darkusha8@gmail.com>
2023-10-15 22:17:57 +0300

    finished development blog

--- mycite/blog/views.py
+++ mycite/blog/views.py
@@ -4,10 +4,30 @@ from django.core.paginator import Paginator, EmptyPage,\
                                             PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import TrigramSimilarity
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request,
+                   'blog/post/search.html',
+                   {'form':form,
+                    'query':query,
+                    'results':results})
 
 @ require_POST
 def post_comment(request, post_id):
