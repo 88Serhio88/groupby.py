0498cad4d660def9266acdd757df4805cc88b8b9
Sergey Zhilenkov <mr.darkusha8@gmail.com>
2023-11-14 23:54:08 +0300

    14.11. add js bookmarklet

--- sonet/images/models.py
+++ sonet/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -16,18 +18,22 @@ class Image(models.Model):
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                         related_name='images_liked',
                                         blank=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
         ]
         ordering = ['-created']
-    
+
     def __str__(self):
         return self.title
-    
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id,
+                                              self.slug])
